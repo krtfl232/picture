57dcaccc81f92e0b1f2fefd3e8015c8147a9ab59
polina <cheburashka697@gmail.com>
2025-08-02 19:44:07 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from PIL import Image,ImageFilter
+with Image.open('i.webp') as pic:
+    other_pic=pic.filter(ImageFilter.BLUR)
+    blur_pic=other_pic.filter(ImageFilter.BLUR)
+    for i in range(5):
+        blur_pic=blur_pic.filter(ImageFilter.BLUR)
+    blur_pic.show()
